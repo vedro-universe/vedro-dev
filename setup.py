ec10609b353556f239056b0dc011733ac7684207
Nikita Tsvetkov <tsv1@fastmail.com>
2023-03-12 23:19:59 +0400

    add github actions

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Nikita Tsvetkov",
     author_email="tsv1@fastmail.com",
-    python_requires=">=3.10",
+    python_requires=">=3.8",
     url="https://github.com/vedro-universe/vedro-dev",
     license="Apache-2.0",
     packages=find_packages(exclude=["tests", "tests.*"]),
@@ -27,11 +27,10 @@ setup(
     install_requires=find_required(),
     tests_require=find_dev_required(),
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Topic :: Software Development",
         "Typing :: Typed",
     ],
 )
